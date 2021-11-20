9e16387696c30b862db97834d95e82a0cf5f8368
LupsterHD <56876250+JakobWeitzlab@users.noreply.github.com>
2021-11-20 17:20:15 +0100

    Klassenstruktur

--- MPU6050-production.py
+++ MPU6050-production.py
@@ -1,55 +1,52 @@
 from smbus2 import SMBus
 import time
 import logging
-import numpy as np
+# import numpy as np
 
-#Adressbereiche
+# Adressbereiche
 adress = 0x68
 POWER_MANAGEMENT_1 = 0x6B
 GYRO_XOUT_H = 0x43
 GYRO_XOUT_L = 0x44
 
-def Initialisierung():
-    #logging Library init
-    logging.basicConfig(level=logging.INFO) #Configure Logging Level
-    return print("init abgeschlossen.")
 
-def WakeUp():
-    with SMBus(1) as bus:
+class MPU():
+
+    def __init__(self):
+        # logging Library init
+        logging.basicConfig(level=logging.INFO)  # Configure Logging Level
+        self._WakeUp()
+        return logging.info("init abgeschlossen.")
+
+    def _WakeUp(self):
+        with SMBus(1) as bus:
             data = 0x00
             bus.write_byte_data(adress, POWER_MANAGEMENT_1, data)
-    return logging.info("MPU Wake up call")
+        return logging.info("MPU Wake up call")
 
-#Read determinated Register
-def RegisterRead(Register_H, Register_L):
-    h = SMBus(1).read_byte_data(adress, Register_H)
-    l = SMBus(1).read_byte_data(adress, Register_L)
-    value = (h << 8) + l
-    return value
+    # Read determinated Register
+    def _RegisterRead(self, Register_H, Register_L):
+        high = SMBus(1).read_byte_data(adress, Register_H)
+        low = SMBus(1).read_byte_data(adress, Register_L)
+        value = (high << 8) + low
+        return value
 
-#Calculate Offset X
-def Offset_X():
-    bus = SMBus(1)
-    gyro_x_5sek = []
-    for i in range(0,1000):
-            value = RegisterRead(GYRO_XOUT_H,GYRO_XOUT_L)
-            
-            if (value >= 32768):
-                int_x = -((65535 - value) + 1)
-            else:
-                int_x = value
+    def _Rotation_x(self):
+        return
 
-            gyro_x_5sek.append(int_x)
-            time.sleep(0.001)
-    bus.close()
-    return print("Mittelwert von X-Achse", np.mean(gyro_x_5sek))
+    def loop(self):
+        '''Execute the Main loop'''
+
+        self._running = True
+        while self._running:
+            time.sleep(1)
 
 
-#MAIN
-try:
-    WakeUp()
-    #do something
-except OSError:
-    print("Kabel ausa zogen")
-    time.sleep(2)
-    
+def main():
+    '''Execute the Main Method'''
+    mpu = MPU()
+    mpu.loop()
+
+
+if __name__ == "__main__":
+    main()
