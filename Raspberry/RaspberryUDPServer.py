6f00e5969548a3aa8d541648ed02dd1719e94bca
LupsterHD <56876250+JakobWeitzlab@users.noreply.github.com>
2022-01-09 21:09:12 +0100

    + messages/s

--- Raspberry/RaspberryUDPServer.py
+++ Raspberry/RaspberryUDPServer.py
@@ -1,18 +1,65 @@
 import socket
-from datetime import datetime
-
-UDP_IP = "192.168.4.1"
-UDP_PORT = 1234
-
-sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
-sock.bind((UDP_IP, UDP_PORT))
+import time
+from functools import partial
 
 
-while True:
-    now = datetime.now()
+class Server:
+    def __init__(self):
 
-    current_time = now.strftime("%H:%M:%S:%f")
+        UDP_IP = "192.168.4.1"
+        UDP_PORT = 1234
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        self.sock.bind((UDP_IP, UDP_PORT))
+        
+    def Receive(self):
+        data, addr = self.sock.recvfrom(1024)  # buffer size is 1024 bytes
+        return data, addr
+
+
+def convertTuple(tup):
+    st = ''.join(map(str, tup))
+    return int(st[10:12])
     
-    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    print("received message: %s" % data ,current_time)
+
+def höhenruder(x, y, z):
+    #print(str(time.time()) + " höhenruder" + str(x)+ str(y)+ str(z))
+    return
+
+def querruder(x, y, z):
+    #print("querruder" + str(x)+ str(y)+ str(z))
+    return
+
+def seitenruder(x, y, z):
+    return
+
+def default():
+    return
+
+switcher = {
+    10: höhenruder,
+    11: querruder,
+    12: seitenruder
+} 
+
+def loop():
+    server = Server()
+    count = 0
+    oldTime = time.time()
+    arr = [] 
+    while True:
+        data, addr = server.Receive()
+        values = data.decode('UTF-8').split(" ")
+        switcher[convertTuple(addr)](str(values[0]), str(values[1]), str(values[2]))
+        arr.append(data)
+        if time.time() >= oldTime + 1:
+            print(str(len(arr)) + " messages/s")
+            oldTime = time.time()
+            arr.clear()
+
+def main():
+    loop()
+
+
+if __name__ == "__main__":
+    main()
