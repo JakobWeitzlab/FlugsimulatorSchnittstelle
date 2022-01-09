1f1588c3ee655d521f802a8cac770681db81f8b8
LupsterHD <56876250+JakobWeitzlab@users.noreply.github.com>
2022-01-09 20:59:08 +0100

    implement cpu freq

--- ESP32/main.py
+++ ESP32/main.py
@@ -2,6 +2,10 @@ from machine import Pin, I2C
 import socket
 import network
 import time
+#import ubinascii
+import machine
+
+
 
 #Wifi Settings
 host='192.168.4.1'
@@ -19,27 +23,41 @@ I2CSlow = 100000
 #Register
 ACCEL_XOUT_H = 0x3B
 ACCEL_XOUT_L = 0x3C
+ACCEL_YOUT_H = 0x3D
+ACCEL_YOUT_L = 0x3E
+ACCEL_ZOUT_H = 0x3F
+ACCEL_ZOUT_L = 0x40
 POWER_MANAGEMENT_1 = 0x6B
+
 
 class MPU():
   def __init__(self):
-    self.i2c = I2C(1, scl=Pin(22), sda=Pin(21), freq=I2CSlow)
+    self.i2c = I2C(1, scl=Pin(22), sda=Pin(21), freq=I2CFast)
     #self._WakeUp()
     self._running = True
     print("Device found on Address " + str(self.i2c.scan()))
-    print("Init MPU complete")
   
   def _WakeUp(self):
     self.i2c.writeto_mem(0x68, 0x6B, bytes([0]))
     print("WakeUp complete")
     time.sleep(0.5)
+    print("Init MPU complete")
     
-  def _RegisterRead(self):
-    high = self.i2c.readfrom_mem(MPUAddress, ACCEL_XOUT_H, 1)
-    low = self.i2c.readfrom_mem(MPUAddress, ACCEL_XOUT_L, 1) 
+  def _RegisterRead(self, Register_H, Register_L):
+    high = self.i2c.readfrom_mem(MPUAddress, Register_H, 1)
+    low = self.i2c.readfrom_mem(MPUAddress, Register_L, 1)
     value = (high[0] << 8) + low[0] #shifting high 8 Bits and adding low
-    #print(bin(value))
-    return(bin(value))
+    return value
+
+  def _Acc(self):
+    t = time.ticks_us()
+    valueX = self._RegisterRead(ACCEL_XOUT_H, ACCEL_XOUT_L)
+    valueY = self._RegisterRead(ACCEL_YOUT_H, ACCEL_YOUT_L)
+    valueZ = self._RegisterRead(ACCEL_ZOUT_H, ACCEL_ZOUT_L)
+    data = str(valueX) + " " + str(valueY) + " " + str(valueZ)
+    delta = time.ticks_diff(time.ticks_us(), t)
+    #print(delta)
+    return data
     
     
 class Wifi():
@@ -53,10 +71,11 @@ class Wifi():
     self.wlan=network.WLAN(network.STA_IF)                 #create a wlan object
     self.wlan.active(True)                                 #Activate the network interface
     self.wlan.disconnect()                                 #Disconnect the last connected WiFi
+    self.wlan.ifconfig(('192.168.4.10', '255.255.255.0', '192.168.4.1', '8.8.8.8')) # IP for Client 1
     self.wlan.connect(ssid,passwd)                         #connect wifi
-    while(self.wlan.ifconfig()[0]=='0.0.0.0'):             #Wait for IP assignment
-      print("Trying to Connect to Wifi")
-      time.sleep(1)
+    while (self.wlan.isconnected() != True):
+        print("trying to connect to Wifi")
+        time.sleep(1)
     return True
     
   def CreateSocket(self):
@@ -73,20 +92,23 @@ class Wifi():
       self.wlan.active(False)
     
   def SendData(self):
-    self.s.sendto(self.mpu._RegisterRead(),(host,port))            #send data
-    print("Data Send")
-    #time.sleep(0.5)
+    self.s.sendto( self.mpu._Acc(),(host,port))            #send data
+    
       
   def loop(self):
     self.mpu = MPU()
     self.mpu._WakeUp()
     
-    for y in range(0,10):
+    while False:
       self.SendData()
-    
-    
+      #self.mpu._Acc()    
+      
+         
 #Execut the Main function    
 def main():
+  return
+  machine.freq(160000000)
+  print(machine.freq())
   wifi = Wifi()
   wifi.loop()
   
